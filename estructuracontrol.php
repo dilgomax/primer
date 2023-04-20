15d09b542d758e22d69058271e675b20b842fa6d
dilgomax <131090926+dilgomax@users.noreply.github.com>
2023-04-20 12:20:49 -0500

    primer

---
+++ estructuracontrol.php
@@ -0,0 +1,40 @@
+<?php
+
+#if else
+
+$saldo = 20000
+
+if ($saldo >=2950) {
+   
+    echo"bienvenido al sistema";
+    $saldo = $saldo - 2950;
+    echo $saldo
+}
+echo "<br>-------------------------------<br>"
+
+
+#elseis
+
+$distancia =16
+
+
+if ($distancia <= 10) {
+    echo "el valor de su envio cuesta 100";
+}
+
+else { $distancia  10 and
+
+}
+
+echo "<br>-------------------------------<br>"
+
+#for
+
+# 
+
+for ($i=0; $i <=10; ; $i++) { 
+    echo "el numero" .$i. "<br>"
+}
+
+
+?>
